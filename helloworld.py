39fdaf4cde56de4f99b8a40c6a81b3357ab2a691
Dan Mello <danielmellosemail@gmail.com>
2019-11-27 22:36:38 -0500

    Added 'Hello World' program.

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+
+# Simple example of a 'Hello World!' program in Python.
+# Author: Dan Mello
+# Version: 1.0
+# Date: 2019-27-11 
+
+# Initialize a string called 'msg'.
+msg = "Hello World!"
+
+# Print the string.
+print(msg)
