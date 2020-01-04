c7fe9112d36343533d53eda57d932baa90788bc6
Dan <danielmellosemail@gmail.com>
2020-01-04 14:52:34 -0500

    Added simple addition program.

---
+++ addition.py
@@ -0,0 +1,17 @@
+
+# Adds two user-input integers and displays the sum
+# Version: 1.0
+# Author: Dan Mello
+# Date: 2020-04-01
+
+# Prompt user to enter first integer.
+x = int(input("Enter Number 1: "))
+
+# Prompt user to enter second integer.
+y = int(input("Enter Number 2: "))
+
+# Add both integers.
+sum = x + y
+
+# Print the sum.
+print(x, " + ", y , " = ", sum)
